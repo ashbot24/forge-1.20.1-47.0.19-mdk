3c2ecdf0a6ab67debaa913772108e89b547ac142
ashbot24 <ash2020gamer@gmail.com>
2024-08-05 12:59:30 +0400

    changes made

--- src/main/java/net/ash/tutorialmod/TutorialMod.java
+++ src/main/java/net/ash/tutorialmod/TutorialMod.java
@@ -1,6 +1,10 @@
 package net.ash.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.ash.tutorialmod.item.ModCreativeModTabs;
+import net.ash.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -26,6 +30,10 @@ public class TutorialMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -43,7 +51,11 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
